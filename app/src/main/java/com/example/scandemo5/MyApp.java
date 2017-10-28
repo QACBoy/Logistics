0adb19f4174f27a27af51b93c14889c7c452c9fd
hicj <hi.cj@outlook.com>
2017-10-28 15:11:35 +0800

    崩溃处理机制

--- app/src/main/java/com/example/scandemo5/MyApp.java
+++ app/src/main/java/com/example/scandemo5/MyApp.java
@@ -3,6 +3,8 @@ package com.example.scandemo5;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.scandemo5.Exception.CrashHandler;
+
 /**
  * Created by Sam on 2017/8/22.
  */
@@ -10,14 +12,23 @@ import android.content.Context;
 public class MyApp extends Application {
 
     private static Context context;
+    private static Application application;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        application = this;
+        //初始化崩溃处理
+        CrashHandler catchHandler = CrashHandler.getInstance();
+        catchHandler.init(getApplicationContext());
     }
 
     public static Context getContext() {
         return context;
+    }
+
+    public static Application getApplication() {
+        return application;
     }
 }
